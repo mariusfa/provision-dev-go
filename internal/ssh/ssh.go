3bec87abb7093456a9e381bdd2f35c45a787688e
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-12 22:41:15 +0200

    Refactor ssh

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -10,30 +10,24 @@ import (
 var publicKey string
 
 func SetupSSH() error {
-	email, err := askForEmail()
-	if err != nil {
-		return err
-	}
 
 	publicKeyPath := os.Getenv("HOME") + "/.ssh/id_ed25519.pub"
 	privateKeyPath := os.Getenv("HOME") + "/.ssh/id_ed25519"
 
 	if _, err := os.Stat(publicKeyPath); os.IsNotExist(err) {
-		// Generate SSH keys using ssh-keygen command
-		fmt.Println("Generating SSH key pair...")
-		cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-C", email, "-f", privateKeyPath, "-N", "")
-		err := cmd.Run()
+		email, err := askForEmail()
 		if err != nil {
-			return fmt.Errorf("failed to generate SSH key: %w", err)
+			return err
+		}
+		if err := generateSSHKey(email, privateKeyPath); err != nil {
+			return err
 		}
 	}
 
-	publicKeyBytes, err := os.ReadFile(publicKeyPath)
-	if err != nil {
-		return fmt.Errorf("failed to read public key: %v", err)
-	}
 
-	publicKey = string(publicKeyBytes)
+	if err := setPublicKey(publicKeyPath); err != nil {
+		return err
+	}
 
 	fmt.Println("Public SSH Key:")
 	fmt.Println(publicKey)
@@ -49,6 +43,26 @@ func askForEmail() (string, error) {
 		return "", fmt.Errorf("failed to read email: %v", err)
 	}
 	return email[:len(email)-1], nil // Remove the newline character
+}
+
+func generateSSHKey(email string, privateKeyPath string) error {
+	fmt.Println("Generating SSH key pair...")
+	cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-C", email, "-f", privateKeyPath, "-N", "")
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to generate SSH key: %w", err)
+	}
+	return nil
+}
+
+func setPublicKey(publicKeyPath string) error {
+	publicKeyBytes, err := os.ReadFile(publicKeyPath)
+	if err != nil {
+		return fmt.Errorf("failed to read public key: %w", err)
+	}
+
+	publicKey = string(publicKeyBytes)
+	return nil
 }
 
 func GetPublicKey() string {
