235740d3db5a3bffa213437ad87ad714bed307f3
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-16 21:01:16 +0200

    setup test skeleton for git

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -3,5 +3,12 @@ package git
 import "testing"
 
 func TestGitSetup(t *testing.T) {
+	// TODO: setup fake user input
+	// TODO: setup fake cli get git config
+	// TODO: setup fake cli setup git config
+	err := SetupGit()
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
 	t.Skip("Not implemented yet")
 }
