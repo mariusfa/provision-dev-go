1e0d3d32e04c276c7894beacf711d2f5fa9e0467
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-20 10:44:37 +0200

    impl check for git config

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,7 +1,29 @@
 package git
 
+import "provision/internal/utils/commandrunner"
+
+var runner commandrunner.ICommandRunner = commandrunner.NewCommandRunner()
+
 func SetupGit() error {
 	// TODO: Implement
 	println("Not implemented yet")
+	isSet, err := isGitConfigSet()
+	if err != nil {
+		return err
+	}
+
+	println(isSet)
 	return nil
+}
+
+func isGitConfigSet() (bool, error) {
+	username, err := runner.RunWithOutput("git", "config", "--get", "user.name")
+	if err != nil {
+		return false, err
+	}
+	email, err := runner.RunWithOutput("git", "config", "--get", "user.email")
+	if err != nil {
+		return false, err
+	}
+	return username != "" && email != "", nil
 }
--- internal/utils/commandrunner/commandrunner.go
+++ internal/utils/commandrunner/commandrunner.go
@@ -4,6 +4,7 @@ import "os/exec"
 
 type ICommandRunner interface {
 	Run(name string, arg ...string) error
+	RunWithOutput(name string, arg ...string) (string, error)
 }
 
 type CommandRunner struct{}
@@ -11,6 +12,15 @@ type CommandRunner struct{}
 func (c *CommandRunner) Run(name string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
 	return cmd.Run()
+}
+
+func (c *CommandRunner) RunWithOutput(name string, arg ...string) (string, error) {
+	cmd := exec.Command(name, arg...)
+	byteOutput, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(byteOutput), nil
 }
 
 func NewCommandRunner() *CommandRunner {
