fb2d7f66f13a668d4d6e3be0e5cfff6474f6ee07
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-20 10:50:56 +0200

    Update git check for isSet

--- internal/git/git.go
+++ internal/git/git.go
@@ -7,23 +7,20 @@ var runner commandrunner.ICommandRunner = commandrunner.NewCommandRunner()
 func SetupGit() error {
 	// TODO: Implement
 	println("Not implemented yet")
-	isSet, err := isGitConfigSet()
-	if err != nil {
-		return err
-	}
+	isSet := isGitConfigSet()
 
 	println(isSet)
 	return nil
 }
 
-func isGitConfigSet() (bool, error) {
+func isGitConfigSet() bool {
 	username, err := runner.RunWithOutput("git", "config", "--get", "user.name")
 	if err != nil {
-		return false, err
+		return false
 	}
 	email, err := runner.RunWithOutput("git", "config", "--get", "user.email")
 	if err != nil {
-		return false, err
+		return false
 	}
-	return username != "" && email != "", nil
+	return username != "" && email != ""
 }
