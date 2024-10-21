8e9b9f59979f00fb805817ea5fc2b41c12f818d8
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-21 20:25:17 +0200

    Finish test for alias

--- internal/alias/alias_test.go
+++ internal/alias/alias_test.go
@@ -2,10 +2,30 @@ package alias
 
 import "testing"
 
-func TestSetupAlias(t *testing.T) {
-	// TODO: setup fakes
+func TestSetupAliasNoAliases(t *testing.T) {
+	aliasesWritten := []string{}
+	writeAliases := func(aliases []string) error {
+		aliasesWritten = aliases
+		return nil
+	}
+	getAliasesFromBash := func() ([]string, error) {
+		return []string{}, nil
+	}
+	getAliasesFromAliasFile := func() ([]string, error) {
+		return []string{
+			"alias ..='cd ..'",
+		}, nil
+	}
 	err := SetupAlias()
 	if err != nil {
 		t.Errorf("Expected nil, got %v", err)
+	}
+
+	if len(aliasesWritten) != 1 {
+		t.Errorf("Expected 1, got %v", len(aliasesWritten))
+	}
+
+	if aliasesWritten[0] != "alias ..='cd ..'" {
+		t.Errorf("Expected 'alias ..='cd ..'', got %v", aliasesWritten[0])
 	}
 }
