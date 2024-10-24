e946934ccd3a7ab2474a6552d6cc10446f3b34c8
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-24 19:30:06 +0200

    Update todo

--- internal/apps/apps.go
+++ internal/apps/apps.go
@@ -2,5 +2,7 @@ package apps
 
 func SetupApps() error {
 	// TODO: Implement
+	// check if home/apps/bin exists else create folder(s)
+	// check if apps/bin are in path, else add to path
 	return nil
 }
