397c5196389a47657f7c385293c3e140c9f3f907
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-28 20:31:54 +0100

    Refactor install neovim

--- internal/neovim/neovim.go
+++ internal/neovim/neovim.go
@@ -34,19 +34,49 @@ var isNeovimInstalled = func() bool {
 }
 
 var installNeovim = func() error {
+	if err := download(); err != nil {
+		return err
+	}
+	if err := extract(); err != nil {
+		return err
+	}
+	if err := symlink(); err != nil {
+		return err
+	}
+	if err := cleanup(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func download() error {
+	println("Downloading neovim")
 	if err := runner.Run("wget", "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"); err != nil {
 		return fmt.Errorf("error downloading neovim: %w", err)
 	}
-	appsFolder := os.Getenv("HOME") + "/apps"
-	if err := runner.Run("tar", "-xvf", "nvim-linux64.tar.gz", "-C", appsFolder); err != nil {
+	return nil
+}
+
+func extract() error {
+	println("Extracting neovim")
+	if err := runner.Run("tar", "-xvf", "nvim-linux64.tar.gz", "-C", os.Getenv("HOME")+"/apps"); err != nil {
 		return fmt.Errorf("error extracting neovim: %w", err)
 	}
+	return nil
+}
 
+func symlink() error {
+	println("Symlinking neovim")
 	neovimBinary := os.Getenv("HOME") + "/apps/nvim-linux64/bin/nvim"
 	symlinkPath := os.Getenv("HOME") + "/apps/bin/nvim"
 	if err := runner.Run("ln", "-s", neovimBinary, symlinkPath); err != nil {
 		return fmt.Errorf("error linking neovim: %w", err)
 	}
+	return nil
+}
+
+func cleanup() error {
+	println("Cleaning up neovim tar file")
 	if err := runner.Run("rm", "nvim-linux64.tar.gz"); err != nil {
 		return fmt.Errorf("error removing neovim tar: %w", err)
 	}
