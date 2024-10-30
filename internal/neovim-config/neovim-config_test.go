47efbc14a17f4ca6cd654f829576442111e2b7c2
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-30 21:25:57 +0100

    Create failing test for neovim config

--- internal/neovim-config/neovim-config_test.go
+++ internal/neovim-config/neovim-config_test.go
@@ -3,5 +3,18 @@ package neovimconfig
 import "testing"
 
 func TestNeovimConfig(t *testing.T) {
-	// TODO: Implement test
+	isNeovimConfigClonedResult := false
+
+	isNeovimConfigSetup = func() bool {
+		return isNeovimConfigClonedResult
+	}
+	cloneNeoviConfig = func() error { return nil }
+
+	if err := SetupNeovimConfig(); err != nil {
+		t.Errorf("NeovimConfig() failed: %v", err)
+	}
+
+	if !isNeovimConfigClonedResult {
+		t.Errorf("NeovimConfig() failed: Neovim config not cloned")
+	}
 }
