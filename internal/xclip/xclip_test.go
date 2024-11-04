cd31cf4d97d0693eb9a62c4c25e31e181d128a5a
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-04 18:12:15 +0100

    Create failing test

--- internal/xclip/xclip_test.go
+++ internal/xclip/xclip_test.go
@@ -3,5 +3,18 @@ package xclip
 import "testing"
 
 func TestSetupXclip(t *testing.T) {
-	// TODO: impl
+	xclipInstalled := false
+
+	isXclipInstalled = func() bool {
+		return xclipInstalled
+	}
+
+	installXclip = func() error {
+		xclipInstalled = true
+		return nil
+	}
+
+	if err := SetupXclip(); err != nil {
+		t.Errorf("SetupXclip() = %v; want nil", err)
+	}
 }
