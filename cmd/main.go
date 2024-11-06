20a6d018a73aece905a5f85ca0981fc5b2278da7
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-06 23:09:45 +0100

    Remove xclip todo

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,6 @@ import (
 )
 
 // TODO: create a custom print util to print with colors and icons
-// TODO: add xclip install, sudo apt install xclip
 // TODO: add c compiler for neovim, sudo apt install build-essential
 // TODO: add help command to print all sub commands
 
