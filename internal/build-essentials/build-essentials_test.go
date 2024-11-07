e4eb3cb05ddbc135d17f26c3bf61ee31bf4deb21
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-07 19:09:11 +0100

    Setup fakes in build essentials

--- internal/build-essentials/build-essentials_test.go
+++ internal/build-essentials/build-essentials_test.go
@@ -3,6 +3,17 @@ package buildessentials
 import "testing"
 
 func TestBuildEssentials(t *testing.T) {
+	beInstalled := false
+
+	isBuildEssentialsInstalled := func() bool {
+		return beInstalled
+	}
+
+	installBuildEssentials := func() error {
+		beInstalled = true
+		return nil
+	}
+
 	if err := SetupBuildEssentials(); err != nil {
 		t.Errorf("SetupBuildEssentials() failed: %v", err)
 	}
