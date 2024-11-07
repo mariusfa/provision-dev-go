4bc63d032039acc7e75e0565aa838a5619d0f8da
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-07 19:12:21 +0100

    Create empty functions for build essentials

--- internal/build-essentials/build-essentials.go
+++ internal/build-essentials/build-essentials.go
@@ -4,3 +4,13 @@ func SetupBuildEssentials() error {
 	// TODO: impl
 	return nil
 }
+
+var isBuildEssentialsInstalled = func() bool {
+	// TODO: impl
+	return false
+}
+
+var installBuildEssentials = func() error {
+	// TODO: impl
+	return nil
+}
