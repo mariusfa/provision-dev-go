44201d70a9b6bee15916d7f8c3c8477be082fc76
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-07 21:17:24 +0100

    Impl is build-essentials installed

--- internal/build-essentials/build-essentials.go
+++ internal/build-essentials/build-essentials.go
@@ -1,13 +1,20 @@
 package buildessentials
 
+import "provision/internal/utils/commandrunner"
+
+var runner commandrunner.ICommandRunner = commandrunner.NewCommandRunner()
+
 func SetupBuildEssentials() error {
 	// TODO: impl
+	println(isBuildEssentialsInstalled())
 	return nil
 }
 
 var isBuildEssentialsInstalled = func() bool {
-	// TODO: impl
-	return false
+	if err := runner.Run("gcc", "--version"); err != nil {
+		return false
+	}
+	return true
 }
 
 var installBuildEssentials = func() error {
