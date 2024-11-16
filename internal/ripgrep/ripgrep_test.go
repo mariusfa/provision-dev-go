3581a36e1589b77ecf1adfc24d058fc3ee8f5bb8
Marius Fagerland <mariusfagerland@yahoo.no>
2024-11-16 11:19:30 +0100

    Impl install ripgrep

--- internal/ripgrep/ripgrep_test.go
+++ internal/ripgrep/ripgrep_test.go
@@ -3,5 +3,22 @@ package ripgrep
 import "testing"
 
 func TestSetupRipgrep(t *testing.T) {
-	// TODO: implement
+	ripgrepInstalled := false
+
+	isRipgrepInstalled = func() bool {
+		return ripgrepInstalled
+	}
+
+	installRipgrep = func() error {
+		ripgrepInstalled = true
+		return nil
+	}
+
+	if err := SetupRipgrep(); err != nil {
+		t.Errorf("SetupRipgrep() = %v; want nil", err)
+	}
+
+	if !ripgrepInstalled {
+		t.Error("ripgrepInstalled not installed")
+	}
 }
