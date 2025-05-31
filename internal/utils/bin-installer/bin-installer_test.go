b9aea53aa75891399c4cb43a9928cd6d46cfe344
mariusfa <mariusfagerland@yahoo.no>
2025-05-31 13:08:39 +0200

    Create empty test for bin-installer

---
+++ internal/utils/bin-installer/bin-installer_test.go
@@ -0,0 +1,36 @@
+package bininstaller
+
+import (
+	"errors"
+	"testing"
+)
+
+type fakeRunner struct {
+	IsInstalled bool
+}
+
+func (f *fakeRunner) Run(name string, arg ...string) error {
+	if name == "curl" && arg[0] == "--version" && f.IsInstalled {
+		return nil
+	}
+	if name == "sudo" && arg[0] == "apt" && arg[1] == "install" && arg[2] == "-y" && arg[3] == "curl" {
+		f.IsInstalled = true
+		return nil
+	}
+
+	return errors.New("command failed")
+}
+
+func (f *fakeRunner) RunWithOutput(name string, arg ...string) (string, error) {
+	return "", nil
+}
+
+func newFakeRunner() *fakeRunner {
+	return &fakeRunner{IsInstalled: false}
+}
+
+func TestBinInstaller(t *testing.T) {
+	// This function is intentionally left empty.
+	// It serves as a placeholder for future tests related to the bininstaller package.
+	// Currently, there are no tests implemented.
+}
